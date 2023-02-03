928936d0f93b579d17ab4ae460b3276420862ab7
egovici <egovici@naver.com>
2023-02-03 23:10:09 +0900

    FEAT: Retrieve class data for timetable inquiry

--- routes/timetable.js
+++ routes/timetable.js
@@ -12,60 +12,69 @@ router.get('/', (req, res)=> {
 
 router.get('/update', async (req, res)=> {
 
-    // const data = JSON.stringify([
-    //     {
-    //             groupType: 20,
-    //             searchYear: 2023,
-    //             searchTerm: 10,
-    //             searchUnivCD: "CS0000",
-    //             searchDeptCD: "CS0200",
-    //     },
-    //  {
-    //             groupType: 20,
-    //             searchYear: 2023,
-    //             searchTerm: 10,
-    //             searchUnivCD: "CP0000",
-    //             searchDeptCD: "CP0610",
-    //     },
-    // ])
-    
-    // const formdata = JSON.stringify({
-    //     "groupType": 20,
-    //     "searchYear": 2023,
-    //     "searchTerm": 10,
-    //     "searchUnivCD": "CS0000",
-    //     "searchDeptCD": "CS0200",
-    // })
+    const searchURL = 'https://info.gachon.ac.kr/Ssu1000q/mainSearch.do'
+    const searchParams = [{
+        'groupType': '20',
+        'searchYear': '2023',
+        'searchTerm': '10',
+        'searchUnivCD': 'CS0000',
+        'searchDeptCD': 'CS0200',
+        'searchIsuCD': '002'
+    },
+    {
+        'groupType': '20',
+        'searchYear': '2023',
+        'searchTerm': '10',
+        'searchUnivCD': 'CP0000',
+        'searchDeptCD': 'CP0610',
+        'searchIsuCD': '002'
+    }];
 
-    const response = await axios.post(
-        'https://info.gachon.ac.kr/Ssu1000q/mainSearch.do',
-        new URLSearchParams({
-            'groupType': '20',
-            'searchYear': '2023',
-            'searchTerm': '10',
-            'searchUnivCD': 'CS0000',
-            'searchDeptCD': 'CS0200',
-            'searchIsuCD': '002'
-        }),
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    for await (const param of searchParams) {
+        await axios.post(
+            searchURL,
+            param,
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                }
             }
-        }
-    )
-    .then((res) => {
-        const filepath = path.join(__dirname, "..\\data");
-        const today = new Date();
-
-        fs.writeFileSync(`${filepath}\\${today.getFullYear() + '-' + (today.getMonth() + 1)+ '-' + today.getDate()+ '-' + (today.getHours() + 1)+ '-' + (today.getMinutes() + 1)+ '-' + (today.getSeconds() + 1)}` + ".json",
-            JSON.stringify(res.data.MainData),
-            "utf-8"
-        );
-    })
-    .catch((err) => console.log(err));
-
+        )
+        .then((res) => {
+            const filepath = path.join(__dirname, "..\\data");
+            const today = new Date();
     
+            fs.writeFileSync(`${filepath}\\${param.searchDeptCD}` + ".json",
+                JSON.stringify(res.data.MainData),
+                "utf-8"
+            );
+        })
+        .catch((err) => {
+            res.status(400).end();
+        })
+    }
+    res.status(200).end();
+    
+})
+
+router.get('/class', async (req, res) => {
+
+    const code = req.query.code;
+    const deptCd = {
+        "CE": "CS0200",
+        "FM": "CP0610"
+    }
+    const filepath = path.join(__dirname, "..\\data");
+    
+    fs.readFile(`${filepath}\\${deptCd[code]}` + ".json", "utf8", (err, data) => {
+        if(err) {
+            console.log(err);
+            res.status(400).end();
+        }else {
+            res.json(data);
+        }
+    });
 })
 
 module.exports = router;
